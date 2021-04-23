50d83944c74d7fe3072b23d888b7d7432a2095e7
Abdelaziz Mohamed (Group) <abdelaziz.mohamed@global.ntt>
2021-04-23 15:02:20 +1000

    python

--- hosts.py
+++ hosts.py
@@ -11,27 +11,27 @@ def main():
     args = parser.parse_args()
    
     if args.list:
-        print(json.dumps({
-    "dyngroup":{
-        "hosts":[
-            "cloud1.cloud.example.com",
-            "cloud2.cloud.example.com"
-        ],
-        "vars":{}
-    },
-    "_meta":{
-        "hostvars":{
-            "cloud1.cloud.example.com":{
-                "type":"web"
+        print({
+            "dyngroup":{
+                "hosts":[
+                    "cloud1.cloud.example.com",
+                    "cloud2.cloud.example.com"
+                ],
+                "vars":{}
             },
-            "cloud2.cloud.example.com":{
-                "type":"database"
+            "_meta":{
+                "hostvars":{
+                    "cloud1.cloud.example.com":{
+                        "type":"web"
+                    },
+                    "cloud2.cloud.example.com":{
+                        "type":"database"
+                    }
+                }
             }
-        }
-    }
-}))
+        })
     else:
-        print(json.dumps({"_meta": {"hostvars": {}}}))
+        print({"_meta": {"hostvars": {}}})
 
 
 if __name__ == '__main__':
